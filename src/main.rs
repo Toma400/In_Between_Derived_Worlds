027fe0026cd330b325faca1c19b5e5926e8fe641
Toma400 <thomasso_calgori@o2.pl>
2023-06-04 11:58:18 +0200

    All changes made to the IBDW

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use bevy::prelude::*;
+
+const dr_ver: f32 = 0.1; // DeRanged version
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome in DeRanged v:{}", dr_ver);
+    App::new().run();
 }
